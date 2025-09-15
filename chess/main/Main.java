7e0ceff1e30af8ae333c9c60138dde5af25e80eb
lluckey3 <spacey_station.4i@icloud.com>
2025-09-14 20:10:49 -0400

    Initial window

---
+++ chess/main/Main.java
@@ -0,0 +1,20 @@
+package chess.main;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    frame.getContentPane().setBackground(Color.black);
+    frame.setLayout(new GridBagLayout());
+    frame.setMinimumSize(new Dimension(600,600));
+    frame.setLocationRelativeTo(null);
+    
+    //Board board = new Board();
+    //frame.add(board);
+    
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
